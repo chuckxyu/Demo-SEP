fdceef4a1c5e68026585b3c67beb048b57c74fe6
Chuck García <carlosgarciaordonez@gmail.com>
2013-10-11 03:48:31 -0500

    profesora, logo sep

--- Resources/ui/common/FirstView.js
+++ Resources/ui/common/FirstView.js
@@ -11,39 +11,52 @@ function FirstView() {
 		left : '85%',
 		width : '16%'
 	});
+
+	var image = Ti.UI.createImageView({
+		image : '/web/kokone-iconos/profesora.png',
+		bottom : '0%',
+		right : '0%'
+	});
+
+	var imageSEP = Ti.UI.createImageView({
+		image : '/web/kokone-iconos/Logo-SEP-peq.png',
+		top : '1%',
+		right : '1%'
+	});
+
 	var btnInicio = Titanium.UI.createButton({
 		title : '',
-		top : 10,
-		height : 71,
-		width : 66,
-		
-		backgroundImage : '/web/kokone-iconos/inicio.png'
+		top : 80,
+		height : 100,
+		width : 80,
+
+		backgroundImage : '/web/kokone-iconos/inicio-s.png'
 
 	});
 
 	var btnCalendar = Titanium.UI.createButton({
 		title : '',
-		top : 91,
-		height : 71,
-		width : 66,
+		top : 200,
+		height : 100,
+		width : 80,
 		backgroundImage : '/web/kokone-iconos/calendario.png'
 
 	});
 
 	var btnRevision = Titanium.UI.createButton({
 		title : '',
-		top : 152,
-		height : 71,
-		width : 66,
+		top : 320,
+		height : 100,
+		width : 80,
 		backgroundImage : '/web/kokone-iconos/revision.png'
 
 	});
 
 	var btnInforme = Titanium.UI.createButton({
 		title : '',
-		top : 213,
-		height : 71,
-		width : 66,
+		top : 440,
+		height : 100,
+		width : 80,
 		backgroundImage : '/web/kokone-iconos/informe.png'
 
 	});
@@ -58,25 +71,40 @@ function FirstView() {
 	btnInicio.addEventListener('click', function(e) {
 		Titanium.API.info("You clicked the button btnInicio");
 		webview.setUrl("http://kokone.inteliglobe-mex.com:8078/portal");
+		btnInicio.setBackgroundImage('/web/kokone-iconos/inicio-s.png');
+		btnCalendar.setBackgroundImage('/web/kokone-iconos/calendario.png');
+		btnRevision.setBackgroundImage('/web/kokone-iconos/revision.png');
+		btnInforme.setBackgroundImage('/web/kokone-iconos/informe.png');
 
 	});
 
 	btnCalendar.addEventListener('click', function(e) {
 		Titanium.API.info("You clicked the button btnCalendar");
 		webview.setUrl("http://www.sep.gob.mx/");
+		btnInicio.setBackgroundImage('/web/kokone-iconos/inicio.png');
+		btnCalendar.setBackgroundImage('/web/kokone-iconos/calendario-s.png');
+		btnRevision.setBackgroundImage('/web/kokone-iconos/revision.png');
+		btnInforme.setBackgroundImage('/web/kokone-iconos/informe.png');
 
 	});
 
 	btnRevision.addEventListener('click', function(e) {
 		Titanium.API.info("You clicked the button btnRevision");
 		webview.setUrl("http://kokone.inteliglobe-mex.com/add/revision.html");
+		btnInicio.setBackgroundImage('/web/kokone-iconos/inicio.png');
+		btnCalendar.setBackgroundImage('/web/kokone-iconos/calendario.png');
+		btnRevision.setBackgroundImage('/web/kokone-iconos/revision-s.png');
+		btnInforme.setBackgroundImage('/web/kokone-iconos/informe.png');
 
 	});
 
 	btnInforme.addEventListener('click', function(e) {
 		Titanium.API.info("You clicked the button btnInforme");
-		webview.setUrl("http://www.adobe.com/content/dam/Adobe/en/devnet/acrobat/pdfs/access.pdf");
-
+		webview.setUrl("http://docs.google.com/viewer?url=http://www.adobe.com/content/dam/Adobe/en/devnet/acrobat/pdfs/access.pdf");
+		btnInicio.setBackgroundImage('/web/kokone-iconos/inicio.png');
+		btnCalendar.setBackgroundImage('/web/kokone-iconos/calendario.png');
+		btnRevision.setBackgroundImage('/web/kokone-iconos/revision.png');
+		btnInforme.setBackgroundImage('/web/kokone-iconos/informe-s.png');
 	});
 
 	scrollView.add(btnInicio);
@@ -85,8 +113,13 @@ function FirstView() {
 	scrollView.add(btnInforme);
 
 	self.add(scrollView);
-
 	self.add(webview);
+	self.add(image);
+	self.add(imageSEP);
+
+	Ti.Gesture.addEventListener('orientationchange', function(e) {
+		Titanium.API.info('Orientation changed');
+	});
 
 	return self;
 }
